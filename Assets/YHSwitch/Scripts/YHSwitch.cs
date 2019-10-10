53c22703d7b8d7af13d084ba90b7400cfe3afac1
Yong Ha <rladydgk0303@naver.com>
2019-10-10 18:18:02 +0900

    #4 스위치의 배경색과 핸들색 변경기능 구현
    스위치 배경색과 핸들색 변경 기능 구현 완료.

--- Assets/YHSwitch/Scripts/YHSwitch.cs
+++ Assets/YHSwitch/Scripts/YHSwitch.cs
@@ -9,22 +9,36 @@ public class YHSwitch : MonoBehaviour
     [Range(0, 3)]
     public float moveDuration = 3f;     // 스위치 이동 애니메이션 시간
 
+    // Color
+    public Color handleColor = Color.white;
+    public Color offBackgroundColor = Color.blue;
+    public Color onBackgroundColor = Color.red;
+
     const float totalHandleMoveLength = 72f;
     const float halfMoveLength = totalHandleMoveLength / 2;
 
-    Image handleImage;                  // 스위치 핸들 이미지
-    Image backgroundImage;              // 스위치 배경 이미지
-    RectTransform handleRectTransform;  // 스위치 핸들 RectTransform
+    Image handleImage;                          // 스위치 핸들 이미지
+    Image backgroundImage;                      // 스위치 배경 이미지
+    RectTransform handleRectTransform;          // 스위치 핸들 RectTransform
 
     // Coroutine
-    Coroutine moveHandleCoroutine;      //핸들 이동 코루틴
+    Coroutine moveHandleCoroutine;              // 핸들 이동 코루틴
+    Coroutine changeBackgroundColorCoroutine;   // 배경색 변경 코루틴
 
     void Start()
     {
         // 핸들 초기화 작업
         GameObject handleObject = transform.Find("Handle").gameObject;
-
         handleRectTransform = handleObject.GetComponent<RectTransform>();
+
+        // Handle Image
+        handleImage = handleObject.GetComponent<Image>();
+        handleImage.color = handleColor;
+
+        // Background Image
+        backgroundImage = GetComponent<Image>();
+        backgroundImage.color = offBackgroundColor;
+
         if (isOn)
             handleRectTransform.anchoredPosition = new Vector2(halfMoveLength, 0);
         else
@@ -40,11 +54,24 @@ public class YHSwitch : MonoBehaviour
 
         float ratio = Mathf.Abs(distance.x) / totalHandleMoveLength;
         float duration = moveDuration * ratio;
+
         if (moveHandleCoroutine != null)
         {
             StopCoroutine(moveHandleCoroutine);
+            moveHandleCoroutine = null;
         }
         moveHandleCoroutine = StartCoroutine(MoveHandle(fromPosition,toPosition,duration));
+        // Background Color Change Coroutine
+
+        Color fromColor = backgroundImage.color;
+        Color toColor = (isOn) ? onBackgroundColor : offBackgroundColor;
+
+        if (changeBackgroundColorCoroutine != null)
+        {
+            StopCoroutine(changeBackgroundColorCoroutine);
+            changeBackgroundColorCoroutine = null;
+        }
+        changeBackgroundColorCoroutine = StartCoroutine(changeBackgroundColor(fromColor,toColor,duration));
     }
     /// <summary>
     /// 핸들을 이동하는 함수
@@ -67,5 +94,26 @@ public class YHSwitch : MonoBehaviour
         }
     }
 
-    // 터치 시 스위치의 배경 색상을 바꿔주는 동작(함수);
+    /// <summary>
+    /// 스위치 배경색 변경 함수
+    /// </summary>
+    /// <param name="fromColor">초기 색상</param>
+    /// <param name="toColor">변경할 색상</param>
+    /// <param name="duration">변경 시간</param>
+    /// <returns>없음</returns>
+    IEnumerator changeBackgroundColor(Color fromColor, Color toColor, float duration)
+    {
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            float t = currentTime / duration;
+            Color newColor = Color.Lerp(fromColor, toColor, t);
+
+            backgroundImage.color = newColor;
+
+
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+    }
 }
